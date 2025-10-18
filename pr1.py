692108c304ee222583784782e5c1900a72a970f7
Adesh <adesh717262+git@gmail.com>
2025-10-18 21:42:57 +0530

    hangman game

--- pr1.py
+++ pr1.py
@@ -1,22 +1,22 @@
-import random as rd
-# GUESS THE NO.
-    # between 1 and 10
-print(f"guess the number between 1 and 10")
-def guess_number(r):
-    count = 0
-    while True:
-        num = int(input(f"Enter a number to guess: "))
-        count += 1
-        if num == r:
-            print(f"You guessed correctly! \n\tIn {count} guesses")
-            break
-        elif num < r:
-            print(f"please enter a number greater than {num}")
-        else:
-            if (num >= 1 and num <= 10):
-                print(f"please enter a number less than {num}")
-            else:
-                print(f"entered no is out of range")
+# import random as rd
+# # GUESS THE NO.
+#     # between 1 and 10
+# print(f"guess the number between 1 and 10")
+# def guess_number(r):
+#     count = 0
+#     while True:
+#         num = int(input(f"Enter a number to guess: "))
+#         count += 1
+#         if num == r:
+#             print(f"You guessed correctly! \n\tIn {count} guesses")
+#             break
+#         elif num < r:
+#             print(f"please enter a number greater than {num}")
+#         else:
+#             if (num >= 1 and num <= 10):
+#                 print(f"please enter a number less than {num}")
+#             else:
+#                 print(f"entered no is out of range")
             
-r = rd.randint(1,10)
-guess_number(r)
+# r = rd.randint(1,10)
+# guess_number(r)
